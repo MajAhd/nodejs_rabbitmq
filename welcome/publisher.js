4038e977acac1c4658589c6869f529125b53a7a7
Majid Ahmaditabar <mjd.ahd.tbr@gmail.com>
2021-09-12 17:52:31 +0430

    pub_sub fle fixed

--- welcome/publisher.js
+++ welcome/publisher.js
@@ -1,17 +1,17 @@
-const amqplib = require("amqplib")
-
-const queue_name = "welcome"
-const msg = "Welcome to RabbitMQ And Nodejs"
+const amqplib = require("amqplib");
+let ts = Date.now();
+const queue_name = "welcome";
+const msg = `Welcome To The RabbitMq`;
 
 const send_message = async () => {
-    const connection = await amqplib.connect('amqp://localhost')
-    const channel = await connection.createChannel()
-    await channel.assertQueue(queue_name, {durable: false})
-    channel.sendToQueue(queue_name, Buffer.from(msg))
-    console.log(`Message send : ${msg}`)
-    setTimeout(() => {
-        connection.close()
-        process.exit(0)
-    }, 500)
-}
-send_message()
+  const connection = await amqplib.connect("amqp://localhost");
+  const channel = await connection.createChannel();
+  await channel.assertQueue(queue_name, { durable: true });
+  channel.sendToQueue(queue_name, Buffer.from(msg));
+  console.log(`Message send : ${msg}`);
+  setTimeout(() => {
+    connection.close();
+    process.exit(0);
+  }, 500);
+};
+send_message();
