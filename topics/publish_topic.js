43a12e000dad29035c483c98b21ef9aabee340e4
Majid Ahmaditabar <mjd.ahd.tbr@gmail.com>
2021-09-14 17:59:47 +0430

    topic exchange update

--- topics/publish_topic.js
+++ topics/publish_topic.js
@@ -1,14 +1,14 @@
 const amqplib = require("amqplib");
 
-const exchange_name = "topic_logs";
+const exchange_name = "error_logs_exchange";
 let args = process.argv.slice(2);
-const key = args[0] || "error.save";
+const key = args[0] || "error.file";
 const msg = args[1] || `Emmit Message ${Math.random()}!`;
 
 const send_message = async () => {
   const connection = await amqplib.connect("amqp://localhost");
   const channel = await connection.createChannel();
-  await channel.assertExchange(exchange_name, "topic", { durable: false }); // <-- topic exchange
+  await channel.assertExchange(exchange_name, "topic", { durable: true }); // <-- topic exchange
   channel.publish(exchange_name, key, Buffer.from(msg)); // <-- publish key
   console.log(`Message send : ${msg}`);
   setTimeout(() => {
--- topics/recieve_topic.js
+++ topics/recieve_topic.js
@@ -1,12 +1,12 @@
 const amqplib = require("amqplib");
 
-const exchange_name = "topic_logs";
+const exchange_name = "error_logs_exchange";
 const key = process.argv.slice(2).join(" ") || "error.#";
 
 const receive_message = async () => {
   const connection = await amqplib.connect("amqp://localhost");
   const channel = await connection.createChannel();
-  await channel.assertExchange(exchange_name, "topic", { durable: false }); // <--- topic
+  await channel.assertExchange(exchange_name, "topic", { durable: true }); // <--- topic
   const q = await channel.assertQueue("", { exclusive: true });
   console.log(`Wait For Message in Queue: ${q.queue}`);
   channel.bindQueue(q.queue, exchange_name, key); // <-- binding key (error)
